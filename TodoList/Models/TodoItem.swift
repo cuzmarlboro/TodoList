fbca647ce578e989d4a85261795e0907e10ee357
hezeying <357261045@qq.com>
2025-04-23 23:11:52 +0800

    feat:本地存储

--- TodoList/Models/TodoItem.swift
+++ TodoList/Models/TodoItem.swift
@@ -8,8 +8,18 @@
 
 import Foundation
 
-struct TodoItem : Identifiable {
-    let id: String = UUID().uuidString
+struct TodoItem : Identifiable, Encodable, Decodable {
+    let id: String
     let title: String
     let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func update() -> TodoItem {
+        return TodoItem(id: self.id, title: self.title, isCompleted: !self.isCompleted)
+    }
 }
--- TodoList/ViewModels/TodoListViewModel.swift
+++ TodoList/ViewModels/TodoListViewModel.swift
@@ -5,25 +5,33 @@
 //  Created by ZHJY on 2025/4/23.
 //
 // 1. init 生命周期函数的执行时机是什么时候？
+// 2. guard 语句是什么意思？
 
 import Foundation
 
 class TodoListViewModel: ObservableObject {
-    @Published var todoItems: [TodoItem] = []
+    @Published var todoItems: [TodoItem] = [] {
+        didSet{
+            saveItem()
+        }
+    }
+    
+    let todoItemsKey: String = "todoItemsKey"
     
     init() {
         initTodoListItems()
     }
     
     func initTodoListItems(){
-        todoItems = [
-            TodoItem(title: "This is the first todo item", isCompleted: false),
-            TodoItem(title: "This is the second todo item", isCompleted: true)
-        ]
+        guard let data = UserDefaults.standard.data(forKey:"todoItemsKey"),
+              let decodeData = try? JSONDecoder().decode([TodoItem].self, from: data)
+        else{ return }
+        todoItems = decodeData
     }
     
     func deleteItem(indexSet: IndexSet){
         todoItems.remove(atOffsets: indexSet)
+        saveItem()
     }
     
     func moveItem(indices: IndexSet, newOffset: Int) {
@@ -33,6 +41,18 @@ class TodoListViewModel: ObservableObject {
     func addItem(title: String) {
         let item = TodoItem(title: title, isCompleted: false)
         todoItems.append(item)
+    }
+    
+    func updateItem(item: TodoItem){
+        let index = todoItems.firstIndex{ $0.id == item.id }!
+        // todoItems[index] = TodoItem(id: item.id, title: item.title, isCompleted: !item.isCompleted )
+        todoItems[index] = todoItems[index].update()
+    }
+    
+    func saveItem() {
+        if let data = try? JSONEncoder().encode(todoItems){
+            UserDefaults.standard.set(data, forKey: todoItemsKey)
+        }
     }
 }
 
